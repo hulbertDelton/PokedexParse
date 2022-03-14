6209b5c701b6bc4be5e6334bae6dfbd45b54d3d9
delton <hulbert.delton@gmail.com>
2022-03-14 17:30:48 -0600

    removing features in utils
    shoddy encapsulation in python has forced me to put a bunch of shit in runtime reeeeee

--- DexUtils.py
+++ DexUtils.py
@@ -1,7 +1,3 @@
-import sys
-import os
-import csv
-
 def parse_dex_entry(p):
     print("\n" + str(p.dex_num) + " - " + p.name)
     print("DEFEATED - " + p.defeat_type + "\nAttacks:")
@@ -15,15 +11,17 @@ def is_int(val):
         return False
     return True
 
-def is_aggro(entry):
+def is_aggro(entry:str):
     attack = "seen it use"
     found = entry.find(attack)
 
     if found == -1:
-        return True
-    else:
         return False
+    else:
+        return True
 
-def all_attacks(pokemon):
-    atks = ""
-    
+def row_add(row_in:int):
+    if row_in % 2 == 0:
+        return row_in + 1
+    else:
+        return row_in
